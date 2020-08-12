1ed86240d5d4eba9ba6b037446395561e98917a6
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-12 14:08:49 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create!(name: "Epicure", category: "french", address:"Rue Oberkampf")
+Restaurant.create!(name: "Luigi", category: "italian", address:"Rue de Mexico")
+Restaurant.create!(name: "Gunther", category: "belgian", address:"Rue de Germany")
+Restaurant.create!(name: "Olé", category: "japanese", address:"Rue de Spain")
+Restaurant.create!(name: "Brit", category: "italian", address:"Rue de UK")
